e768d26423acd2e0f4af1c62b3d2abb2d018b8cc
choiminjun <chlalswns200@naver.com>
2025-05-24 13:54:56 +0900

    Add Swagger settings

---
+++ src/main/java/com/example/jpashopdemo/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.example.jpashopdemo.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Product API")
+                .version("v1.0")
+                .description("상품 관리 시스템 API 문서")
+                .termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org"));
+
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .info(info);
+    }
+}
